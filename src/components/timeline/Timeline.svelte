5d2cd65b17f20ac77e98fc6fd2f98cf86a52acf7
sittichok Ouamsiri <sutichok46@gmail.com>
2024-01-09 01:24:56 +0700

    Timeline (#3)

--- src/components/timeline/Timeline.svelte
+++ src/components/timeline/Timeline.svelte
@@ -1,3 +1,123 @@
-<div>
-	<h1>Component here</h1>
+<script>
+	import { t } from '$lib/translation/translations';
+</script>
+
+<div class="wrapper flex w-full justify-center flex-col">
+	<h3 class="text-subheader font-bold text-red500 text-center">{$t('home.timelineSubtitle')}</h3>
+	<h2 class="text-h2 font-bold text-center">{$t('home.timelineTitle')}</h2>
+	<div class="w-full aspect-[1056/440] relative">
+		<svg
+			class="w-full h-full"
+			viewBox="0 0 1029 442"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M1.99999 1V0H0.836655L1.01133 1.15015L1.99999 1ZM1027.19 1L1028.18 1.15015L1028.36 0H1027.19V1ZM1025.17 1V0H1024.31L1024.18 0.849267L1025.17 1ZM4.02302 1L5.01159 0.849267L4.8821 0H4.02302V1ZM514.596 440C255.424 440 40.7483 249.478 2.98866 0.84985L1.01133 1.15015C38.9172 250.741 254.42 442 514.596 442V440ZM1026.2 0.849849C988.444 249.478 773.769 440 514.596 440V442C774.772 442 990.275 250.741 1028.18 1.15015L1026.2 0.849849ZM1027.19 0H1025.17V2H1027.19V0ZM514.596 440C773.664 440 988.271 249.632 1026.16 1.15073L1024.18 0.849267C986.44 248.368 772.661 438 514.596 438V440ZM3.03445 1.15073C40.9216 249.632 255.528 440 514.596 440V438C256.531 438 42.7521 248.368 5.01159 0.849267L3.03445 1.15073ZM4.02302 0H1.99999V2H4.02302V0Z"
+				fill="url(#paint0_linear_137_39)"
+			/>
+			<defs>
+				<linearGradient
+					id="paint0_linear_137_39"
+					x1="514.596"
+					y1="1"
+					x2="514.596"
+					y2="441"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop offset="0.125" stop-color="#4285F4" />
+					<stop offset="0.541667" stop-color="#34A853" />
+					<stop offset="0.885417" stop-color="#F9AB00" />
+					<stop offset="1" stop-color="#EA4335" />
+				</linearGradient>
+			</defs>
+		</svg>
+		<div
+			class="w-[7%] aspect-square p-[5px] md:p-3 left-0 top-[9%] absolute bg-slate-50 rounded-full border border-black"
+		>
+			<svg
+				class="w-full aspect-square"
+				viewBox="0 0 39 33"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M26.7969 0C24.3121 0 22.1684 1.41863 20.8529 3.56684C23.0454 5.75558 24.3608 8.75497 24.3608 12.2002C24.3608 13.3756 24.2147 14.4295 23.9223 15.4833C24.8481 15.9292 25.7738 16.2534 26.7969 16.2534C30.8408 16.2534 34.1052 12.6055 34.1052 8.14698C34.1052 3.68843 30.8408 0.0405322 26.7969 0.0405322V0ZM12.1804 4.05323C8.13652 4.05323 4.87217 7.70113 4.87217 12.1597C4.87217 16.6182 8.13652 20.2661 12.1804 20.2661C16.2243 20.2661 19.4887 16.6182 19.4887 12.1597C19.4887 7.70113 16.2243 4.05323 12.1804 4.05323ZM35.3232 16.8614C33.2282 18.9286 30.3536 20.1851 27.0893 20.2661C28.4047 21.8064 29.233 23.6708 29.233 25.6974V28.3726H38.9773V21.6442C38.9773 19.5365 37.467 17.7126 35.3232 16.8209V16.8614ZM3.65413 20.9146C1.51037 21.8064 0 23.6303 0 25.738V32.4663H24.3608V25.738C24.3608 23.6303 22.8505 21.8064 20.7067 20.9146C18.5142 23.0628 15.5422 24.3194 12.1804 24.3194C8.81862 24.3194 5.8466 23.0223 3.65413 20.9146Z"
+					fill="black"
+				/>
+			</svg>
+		</div>
+		<div
+			class="w-[7%] aspect-square p-[5px] md:p-3 right-0 top-[9%] absolute bg-slate-50 rounded-full border border-black"
+		>
+			<svg
+				class="w-full aspect-square"
+				viewBox="0 0 38 34"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<g id="&#240;&#159;&#166;&#134; icon &#34;bar chart&#34;">
+					<path
+						id="Vector"
+						d="M0 0V33.25H38V28.5H4.75V0H0ZM23.75 0V23.75H33.25V0H23.75ZM9.5 9.5V23.75H19V9.5H9.5Z"
+						fill="black"
+					/>
+				</g>
+			</svg>
+		</div>
+		<div
+			class="w-[7%] aspect-square p-[5px] md:p-3 left-[20%] top-[72%] absolute bg-slate-50 rounded-full border border-black"
+		>
+			<svg
+				class="w-full aspect-square"
+				viewBox="0 0 39 34"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<g id="&#240;&#159;&#166;&#134; icon &#34;calendar&#34;">
+					<path
+						id="Vector"
+						d="M0.0556348 0V8.5H39V0H0.0556348ZM0.0556348 12.75V33.6175C0.0556348 33.83 0.278174 34 0.556348 34H38.4437C38.7218 34 38.9444 33.83 38.9444 33.6175V12.75H0H0.0556348ZM5.61912 17H11.1826V21.25H5.61912V17ZM16.7461 17H22.3096V21.25H16.7461V17ZM27.873 17H33.4365V21.25H27.873V17ZM5.61912 25.5H11.1826V29.75H5.61912V25.5ZM16.7461 25.5H22.3096V29.75H16.7461V25.5Z"
+						fill="black"
+					/>
+				</g>
+			</svg>
+		</div>
+		<div
+			class="w-[7%] aspect-square p-[5px] md:p-3 left-[73%] top-[72%] absolute bg-slate-50 rounded-full border border-black"
+		>
+			<svg
+				class="w-full aspect-square"
+				viewBox="0 0 39 34"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<g id="&#240;&#159;&#166;&#134; icon &#34;task&#34;">
+					<path
+						id="Vector"
+						d="M0 0V34H34V16.5629L29.1429 21.42V29.1429H4.85714V4.85714H22.2943L27.1514 0H0ZM34 0L19.4286 14.5714L14.5714 9.71429L9.71429 14.5714L19.4286 24.2857L38.8571 4.85714L34 0Z"
+						fill="black"
+					/>
+				</g>
+			</svg>
+		</div>
+		<div class="left-[9%] top-[12%] absolute">
+			<p class=" text-blue-500 text-[10px] md:text-sm font-medium">{$t('home.timeline0Date')}</p>
+			<p class=" text-black text-sm md:text-lg font-bold">{$t('home.timeline0Title')}</p>
+		</div>
+		<div class="right-[9%] top-[12%] absolute">
+			<p class="text-blue-500 text-[10px] md:text-sm font-medium">{$t('home.timeline3Date')}</p>
+			<p class=" text-black text-sm md:text-lg font-bold">{$t('home.timeline3Title')}</p>
+		</div>
+		<div class="left-[23%] top-[40%] sm:top-[50%] md:top-[55%] absolute">
+			<p class=" text-blue-500 text-[10px] md:text-sm font-medium">{$t('home.timeline1Date')}</p>
+			<p class="text-black text-sm md:text-lg font-bold">{@html $t('home.timeline1Title')}</p>
+		</div>
+		<div class="right-[23%] top-[40%] sm:top-[50%] md:top-[55%] absolute">
+			<p class=" text-blue-500 text-[10px] md:text-sm font-medium">{$t('home.timeline2Date')}</p>
+			<p class=" text-black text-sm md:text-lg font-bold">
+				{@html $t('home.timeline2Title')}
+			</p>
+		</div>
+	</div>
 </div>
