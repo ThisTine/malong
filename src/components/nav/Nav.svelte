006292724cbf0b601ba9bc2339d796fd389c620f
fxh <90613107+fxhPhxrxchxyx@users.noreply.github.com>
2024-01-10 12:35:49 +0700

    Nav ver2 (#6)

--- src/components/nav/Nav.svelte
+++ src/components/nav/Nav.svelte
@@ -52,11 +52,7 @@ import { t } from '$lib/translation/translations';
   img.icon {
     margin-left: 5px;
   }
-  .nav-container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 0 15px;
-}
+
 
 
 
@@ -69,13 +65,17 @@ import { t } from '$lib/translation/translations';
   cursor: pointer;
 }
 .logo {
-  display: block;
   margin: 0 auto;
 }
 
-/* navbar side*/
- @media (max-width: 768px) {
-.mobile-menu {
+
+.nav-links{
+column-gap: 50px;
+}
+
+@media (max-width: 768px) {
+ /* navbar side*/
+  .mobile-menu {
   position: fixed;
   top: 0;
   left: 0%;
@@ -91,19 +91,15 @@ import { t } from '$lib/translation/translations';
   justify-content: center;
   gap: 30px;
 }
- }
-.nav-links{
-column-gap: 50px;
-}
 
-@media (max-width: 768px) {
   /* Hide the regular nav-links in mobile view */
   .nav-links {
     display: none;
   }
   /* Show the hamburger menu */
   .hamburger {
-    display: block;
+    display: flex;
+    justify-content: flex-start;
   }
   .close{
 	display: flex;
@@ -111,36 +107,71 @@ column-gap: 50px;
 	margin-top: 120px;
 	width: 30px;
   }
-  
+    .nav-container {
+  padding: 0 15px;
+
+}
+
+.logo{
+  display: flex;
+  justify-content: center;
+}
+
+.hamburger{
+  margin-right: 15px;
+}
 }
 
 @media (min-width: 1080px){
 	.mobile-menu{
 		display: none;
 	}
+
+
+   .nav-container {
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content:flex-start ;
 }
 
+  .logo{
+  display: flex;
+  justify-content: flex-start;
+}
+ 
+ 
+}
+
+.navbg{
+  background-color: #FBFCFF;
+  /* padding: 2px 0; */
+  position: fixed;
+  width: 100%;
+}
 
 </style>
 
 <nav>
-  <div class="wrapper flex w-full justify-center mt-5 mb-5" >
-	 <div class="nav-container flex justify-between items-center w-full">
+  <div class="navbg">
+  <div class="wrapper flex w-full mt-5 mb-5" >
+	 <div class="nav-container flex">
 	  <button class="hamburger" on:click={() => (showMobileMenu = !showMobileMenu)}>
        <img src="burger.svg" alt="Menu" />
       </button>
-    <img class="logo"  alt="Logo" src="logo.png" style="width:100px" />
-
+    <div class="logo"  >
+    <img alt="Logo" src="logo.png" style="width:100px;" />
+    </div>
 	</div>
   
 	{#if showMobileMenu}
       <div class="mobile-menu">
-      <a href="#overview"><p>{$t('home.overview')}</a>
-      <a href="#faq">{$t('home.faqs')}</a>
-      <a href="#timeline">{$t('home.timeline')}</a>
-      <a href="#about">{$t('home.about')}</a>
+      <a class="text-black font-normal" href="#overview"><p>{$t('home.overview')}</a>
+      <a class="text-black font-normal" href="#faq">{$t('home.faqs')}</a>
+      <a class="text-black font-normal" href="#timeline">{$t('home.timeline')}</a>
+      <a class="text-black font-normal" href="#about">{$t('home.about')}</a>
 	  <div>
-      	<div class="dropdown onlyMobile" >
+      	<div class="dropdown onlyMobile text-black font-normal" >
         <button on:click={toggleDropdown}>
           {$t('home.lang')}
           <img src="down.svg" alt="Down" class="icon" />
@@ -160,12 +191,12 @@ column-gap: 50px;
    
 	  <div  class="nav-links wrapper flex w-full justify-center " >
       <div style="display: flex; justify-content: center; gap:30px;">
-      <a href="#overview"><p>{$t('home.overview')}</a>
-      <a href="#faq">{$t('home.faqs')}</a>
-      <a href="#timeline">{$t('home.timeline')}</a>
-      <a href="#about">{$t('home.about')}</a>
+      <a class="text-black font-normal" href="#overview"><p>{$t('home.overview')}</a>
+      <a class="text-black font-normal" href="#faq">{$t('home.faqs')}</a>
+      <a class="text-black font-normal" href="#timeline">{$t('home.timeline')}</a>
+      <a class="text-black font-normal" href="#about">{$t('home.about')}</a>
 	  <div>
-      	<div class="dropdown" >
+      	<div class="dropdown text-black font-normal" >
         <button on:click={toggleDropdown}>
           {$t('home.lang')}
           <img src="down.svg" alt="Down" class="icon" />
@@ -180,6 +211,7 @@ column-gap: 50px;
 	    </div>
 
     </div>
+  </div>
   </div>
  </nav>
 
