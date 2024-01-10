99fec306049007cb49b5f75637b13d6eb2696a8e
sittichok Ouamsiri <sutichok46@gmail.com>
2024-01-11 01:34:15 +0700

    feat: languagebutton,bg

--- src/routes/[language]/+page.svelte
+++ src/routes/[language]/+page.svelte
@@ -13,11 +13,14 @@
 	import Sponsors from '../../components/sponsors/Sponsors.svelte';
 	import SDG from '../../components/sdg/Sdg.svelte';
 	import Nav from '../../components/nav/Nav.svelte';
+
+	/** @types import("./$types").PageData */
+	export let data;
 </script>
 
 <header>
 	<!--Fxh-->
-	<Nav />
+	<Nav lang={data.lang} />
 </header>
 <main>
 	<!--Fxh-->
