acd639817d1b5cadb71b6f3424221c9912258f79
Eve-nichakon <hello.eve555@gmail.com>
2024-01-11 16:08:26 +0700

    update: translation

--- src/components/aboutgdsc/AboutGdsc.svelte
+++ src/components/aboutgdsc/AboutGdsc.svelte
@@ -4,7 +4,7 @@
 
 <div class="wrapper flex w-full justify-center flex-col">
 	<h5 class="text-subheader text-green-500 font-bold text-center">{$t('home.aboutusSubheader')}</h5>
-	<h2 class="text-h2 font-bold text-center">About us</h2>
+	<h2 class="text-h2 font-bold text-center">{$t('home.aboutusHeader')}</h2>
 	<div class="layout">
 		<p class="text-subheader text-black font-bold club"><span class="gdsc">Google Developer Student Clubs Thailand</span> (GDSC - Thailand)</p>
 		<p class="text-subheader text-black font-normal">{$t('home.aboutusBody')}</p>
