8a6fefcbfd06ddf011c9e92ab00184ac92ed2c80
Eve-nichakon <hello.eve555@gmail.com>
2024-01-12 03:07:16 +0700

    add: Chapter section
    thing left:
    - center GDSC MU component
    - Change translate for header
    - add full name of uni below gdsc logo

--- src/components/chapters/Chapters.svelte
+++ src/components/chapters/Chapters.svelte
@@ -1,3 +1,34 @@
-<div>
-	<h1>Agenda</h1>
+<script lang="ts">
+	import { t } from '$lib/translation/translations';
+	import ChapterCard from './ChapterCard.svelte';
+
+	let chapters:Chapter[] = [{
+		name:"GDSC CU",
+	},{
+		name:"GDSC TU",
+	},{
+		name:"GDSC KU",
+	},{
+		name:"GDSC KKU",
+	},{
+		name:"GDSC KMUTT",
+	},{
+		name:"GDSC KMITL",
+	},{
+		name:"GDSC MU",
+	}]
+</script>
+
+<div class="wrapper flex w-full justify-center flex-col">
+	<h5 class="text-subheader text-red-500 font-bold text-center">{$t('home.chapterSubheader')}</h5>
+	<h2 class="text-h2 font-bold text-center">{$t('home.chapterHeader')}</h2>
+	 <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5 pb-10 justify-center">
+  {#each chapters as chapter}
+    <ChapterCard chapter={chapter} />
+	{/each} 
+	</div>
+
 </div>
+
+<style>
+</style>
