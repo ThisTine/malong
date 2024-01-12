6bfb8e462ffbab923c305f078a0b87cfd9f3ee2f
Eve-nichakon <hello.eve555@gmail.com>
2024-01-12 09:48:04 +0700

    update: alignment of chapters

--- src/components/chapters/Chapters.svelte
+++ src/components/chapters/Chapters.svelte
@@ -19,14 +19,24 @@
 	}]
 </script>
 
-<div class="wrapper flex w-full justify-center flex-col">
+<div class="wrapper flex w-full justify-center flex-col my-10">
 	<h5 class="text-subheader text-red-500 font-bold text-center">{$t('home.chapterSubheader')}</h5>
 	<h2 class="text-h2 font-bold text-center">{$t('home.chapterHeader')}</h2>
-	 <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5 pb-10 justify-center">
-  {#each chapters as chapter}
-    <ChapterCard chapter={chapter} />
+
+		
+	<div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5 pb-10 justify-center">
+    {#each chapters as chapter}
+		{#if chapter.name != "GDSC MU"}
+			<ChapterCard chapter={chapter} />
+		{:else}
+			<div class="grid grid-cols-subgrid gap-5 col-span-3">
+    			<div class="col-start-2">
+					<ChapterCard chapter={chapter} />
+				</div>
+  			</div>
+		{/if}
 	{/each} 
-	</div>
+</div>
 
 </div>
 
