531204e1c52373ccdaa7bc2fa5c8af7af6ba2e8e
Nattawat Teaiwtan <98137566+KNattawat89@users.noreply.github.com>
2024-01-14 14:42:36 +0700

    add: faq component (#27)

--- src/components/faq/FAQ.svelte
+++ src/components/faq/FAQ.svelte
@@ -1,3 +1,30 @@
-<div>
-	<h1>Component here</h1>
+<script>
+	import { t } from '$lib/translation/translations';
+	import FAQ from './FAQCard.svelte';
+</script>
+
+<div class="container wrapper flex flex-col items-center mb-10 max-w-818">
+	<div class="text-subheader font-bold text-yellow-500">{$t('home.faq')}</div>
+	<h1 class="text-h2 font-bold">{$t('home.titleFaq')}</h1>
+	<div class="faq-container">
+		<FAQ faq={{ question: $t('home.q1Faq'), answer: $t('home.a1Faq') }} />
+		<FAQ faq={{ question: $t('home.q2Faq'), answer: $t('home.a2Faq') }} />
+		<FAQ faq={{ question: $t('home.q3Faq'), answer: $t('home.a3Faq') }} />
+		<FAQ faq={{ question: $t('home.othersFaq'), answer: $t('') }} />
+	</div>
+	<br />
 </div>
+
+<style>
+	.container {
+		@media only screen and (max-width: 1024px) {
+			padding: 20px;
+		}
+	}
+
+	.faq-container {
+		width: 100%;
+		max-width: 818px;
+		margin: 0 auto;
+	}
+</style>
