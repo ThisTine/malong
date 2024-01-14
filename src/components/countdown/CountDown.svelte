8ac39b2386b798127a25d8401813a9ad9172314e
Anner <icetherockth@gmail.com>
2024-01-14 11:13:23 +0700

    #feat: format timer

--- src/components/countdown/CountDown.svelte
+++ src/components/countdown/CountDown.svelte
@@ -1,7 +1,6 @@
-
 <script>
 	import { onMount } from "svelte";
-	import { t } from '$lib/translation/translations';
+	import { t, locale } from '$lib/translation/translations';
 
 	let endDate = new Date("2024-02-10T00:00:00");
 	let days = 0,
@@ -22,6 +21,14 @@
 		days = hours = minutes = seconds = 0;
 	  }
 	}
+
+	function formatTime(value, unit) {
+    if ($locale === 'en') {
+      return `${unit}${value === 1 || value === 0 ? '' : 's'}`;
+    } else {
+      return `${unit}`;
+    }
+  }
   
 	onMount(() => {
 	  updateCountdown();
@@ -44,22 +51,22 @@
 	  <div class="flex flex-row gap-12 pt-10">
 		<div class="flex flex-col justify-center items-center">
 		  <h1 class="text-[2.5rem]">{days}</h1>
-		  <div class="text-[1rem]">{$t('home.day')}</div>
+		  <div class="text-[1rem]">{formatTime(seconds, $t('home.day'))}</div>
 		</div>
 		<div class="border-2 border-[#4285F4]"></div>
 		<div class="flex flex-col justify-center items-center">
 		  <h1 class="text-[2.5rem]">{hours}</h1>
-		  <div class="text-[1rem]">{$t('home.hour')}</div>
+		  <div class="text-[1rem]">{formatTime(seconds, $t('home.hour'))}</div>
 		</div>
 		<div class="border-2 border-[#34A853]"></div>
 		<div class="flex flex-col justify-center items-center">
 		  <h1 class="text-[2.5rem]">{minutes}</h1>
-		  <div class="text-[1rem]">{$t('home.minute')}</div>
+		  <div class="text-[1rem]">{formatTime(seconds, $t('home.minute'))}</div>
 		</div>
 		<div class="border-2 border-[#F9AB00]"></div>
 		<div class="flex flex-col justify-center items-center">
 		  <h1 class="text-[2.5rem]">{seconds}</h1>
-		  <div class="text-[1rem]">{$t('home.second')}</div>
+		  <div class="text-[1rem]">{formatTime(seconds, $t('home.second'))}</div>
 		</div>
 	  </div>
 	</div>
