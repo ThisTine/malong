e6eb2caa55e5a646d9ec518935e77e5e741867f8
Eve-nichakon <hello.eve555@gmail.com>
2024-01-14 23:15:47 +0700

    update: gdsc logo

--- src/components/chapters/ChapterCard.svelte
+++ src/components/chapters/ChapterCard.svelte
@@ -1,24 +1,24 @@
 <script>
 	import { t } from '$lib/translation/translations';
 
-/** @type {Chapter}*/
+	/** @type {import('../../types/chapter').Chapter}*/
 	export let chapter;
 </script>
 
 <div class="chapter-card flex flex-col justify-between items-center">
-	<img src='/gdsc-logo.png' alt={chapter.name}/>
-    <h2 class="text-subheader font-bold mt-10">{chapter.name}</h2>
+	<img src={chapter.logo} alt={chapter.name} />
+	<h2 class="text-subheader font-bold mt-3">{chapter.name}</h2>
 </div>
 
 <style>
-	.chapter-card{
+	.chapter-card {
 		/* max-width: 380px; */
-		height: 252px; 
+		height: 252px;
 		padding: 1.5rem;
-        padding-top: 3rem;
+		padding-top: 3rem;
 		border-radius: 16px;
-border: 1px solid #F1F3F4;
-background: #ffffff;
-box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.15);
-    }
+		border: 1px solid #f1f3f4;
+		background: #ffffff;
+		box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.15);
+	}
 </style>
