7b60a8b9f8448f84c2981fecc69315fdc846cc2b
sittichok Ouamsiri <sutichok46@gmail.com>
2024-01-21 18:03:54 +0700

    fix: map

--- src/components/venue/Venue.svelte
+++ src/components/venue/Venue.svelte
@@ -12,12 +12,13 @@
 
 		<div class="flex justify-center h-full w-4/5 min-h-[400px] mt-5">
 			<div class="flex w-full">
-					<iframe
-						src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15480.253381654531!2d100.602876!3d14.0734367!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e27fecb1e8d73f%3A0xe114a07c97a9a674!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LiY4Lij4Lij4Lih4Lio4Liy4Liq4LiV4Lij4LmMIOC4qOC4ueC4meC4ouC5jOC4o-C4seC4h-C4quC4tOC4lQ!5e0!3m2!1sth!2sth!4v1705212686557!5m2!1sth!2sth"
-						frameborder="0"
-						allowfullscreen
-						referrerpolicy="no-referrer-when-downgrade"
-						class="w-full h-full rounded-2xl"></iframe>
+				<iframe
+					title="map"
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3870.092429012124!2d100.6046122553505!3d14.07171902119926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e27f69d73d56e7%3A0x5a1cc2df12294899!2sFaculty%20of%20Learning%20Sciences%20and%20Education%2C%20Thammasat%20University!5e0!3m2!1sen!2sth!4v1705834792481!5m2!1sen!2sth"
+					allowfullscreen
+					referrerpolicy="no-referrer-when-downgrade"
+					class="w-full h-full rounded-2xl"
+				></iframe>
 			</div>
 		</div>
 	</div>
