e133f552566f6ebb2565da46cb6de1de67e1981a
sittichok Ouamsiri <sutichok46@gmail.com>
2024-01-21 20:31:34 +0700

    fix: sponsor aspect

--- src/components/sponsors/SponsorCard.svelte
+++ src/components/sponsors/SponsorCard.svelte
@@ -1,23 +1,25 @@
 <script>
 	import { t } from '$lib/translation/translations';
 
-/** @type {import('../../types/sponsor').Sponsor}*/
+	/** @type {import('../../types/sponsor').Sponsor}*/
 	export let sponsor;
 </script>
 
 <div class="sponsor-card flex flex-col justify-center items-center">
-	<img src={sponsor.image} alt={sponsor.name}/>
-    <h2 class="text-subheader font-bold mt-4">{sponsor.name}</h2>
+	<div class="aspect-square flex items-center">
+		<img src={sponsor.image} alt={sponsor.name} />
+	</div>
+	<h2 class="text-subheader font-bold mt-4">{sponsor.name}</h2>
 </div>
 
 <style>
-	.sponsor-card{
+	.sponsor-card {
 		/* max-width: 380px;
 		max-height: 250px; */
 		padding: 1.5rem;
 		border-radius: 16px;
-border: 1px solid #F1F3F4;
-background: #ffffff;
-box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.15);
-    }
+		border: 1px solid #f1f3f4;
+		background: #ffffff;
+		box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.15);
+	}
 </style>
